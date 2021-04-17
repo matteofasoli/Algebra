19a48d54bcde6b4cc912ff6c1c2b3db185f6ec76
fasolimatteo <fasolimatteo9@gmail.com>
2021-04-17 15:10:13 +0200

    test funzionali

---
+++ geometria/src/test/java/testfunzionale/Testprova.java
@@ -0,0 +1,33 @@
+package testfunzionale;
+
+import static org.junit.Assert.*;
+
+import java.time.Duration;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+
+public class Testprova {
+
+	@Test
+	public void functionaltest() {
+        WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        try {
+            driver.get("https://google.com/ncr");
+            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+            System.out.println(firstResult.getAttribute("textContent"));
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
